6f223a3eba6047f4982939a71fd47a3d22817cfa
Anshul Agrawal <anshul3110@gmail.com>
2019-10-17 17:22:07 +0530

    Controller and welcome page added

---
+++ src/main/java/com/anshulsblog/service/example/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.anshulsblog.service.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	private String message = "Hello Friends!! Welcome to the world of Spring Boot.";
+	 
+	 @RequestMapping("/")
+	 public String welcome(ModelMap model) {
+	  model.addAttribute("message", this.message);
+	  return "welcome";
+	 }
+
+}
